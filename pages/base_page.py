5b59a3d1ab453462dfe26b3c2a55b346c38a80cd
Amir <ronigats@mail.ru>
2026-04-03 21:24:25 +0300

    Add BasePage

---
+++ pages/base_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class BasePage:
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    def open(self, url: str):
+        self.driver.get(url)
+
+    def find(self, locator):
+        return self.driver.find_element(*locator)
+
+    def click(self, locator):
+        self.find(locator).click()
+
+    def type(self, locator, text: str):
+        element = self.find(locator)
+        element.clear()
+        element.send_keys(text)
+
+    def get_text(self, locator):
+        return self.find(locator).text
+
+    def is_displayed(self, locator):
+        return self.find(locator).is_displayed()
